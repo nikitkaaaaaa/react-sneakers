8a80c814b58835a792de2a445d748534c733bb9e
nikitka <nikitahyesos@gmail.com>
2024-09-04 18:34:06 +0300

    создал проект

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../style/global.css";
+
+const App = () => {
+  return (
+    <div className="font-b">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quod
+      fugit ut veritatis rem perspiciatis enim? Odit atque ipsum iure
+      repellendus molestias, incidunt facere blanditiis quod velit eius
+      necessitatibus laboriosam?
+    </div>
+  );
+};
+
+export default App;
